acb3dceb4d67412e584356eab253cb9df5d292dd
Marcelo Ramalho Oliveira <marcelo.ramalho-oliveira@outlook.com>
2026-03-16 21:12:40 -0300

    exercicios de dicionarios

---
+++ desafios_dicionarios_2.py
@@ -0,0 +1,13 @@
+alunos = {
+    "Ana": [8,7,9],
+    "Carlos": [5,6,7],
+    "João": [10,9,8],
+    "Maria": [6,7,8]
+}
+
+#media de cada aluno
+
+for media in alunos.items():
+
+    for calcMedia in media.items():
+        
